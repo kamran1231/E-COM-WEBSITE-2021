04c40088f98cfb5154464cd44fde677d86c28c38
kamran <khanbrother805@gmail.com>
2021-07-02 00:45:03 +0530

    7th Commit - Cart Counter

--- Store/views.py
+++ Store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 from category.models import Category
+from Cart.models import CartItem
+from Cart.views import _cart_id
 # Create your views here.           
 
 def store(request,category_slug=None):
@@ -24,9 +26,13 @@ def store(request,category_slug=None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),
+                                          product=single_product).exists()
+
     except Exception as e:
         raise e
     context = {
-        'single_product':single_product
+        'single_product':single_product,
+        'in_cart':in_cart
     }
     return render(request,'store/product_detail.html',context)
